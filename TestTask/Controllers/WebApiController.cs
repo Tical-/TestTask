3378ea4c57f3661954b09a36e1500f0c3e6a08bd
Sanzhar <sanjars@live.com>
2017-12-04 20:10:03 +0600

    1

--- TestTask/Controllers/WebApiController.cs
+++ TestTask/Controllers/WebApiController.cs
@@ -22,15 +22,26 @@ namespace TestTask.Controllers
         public IEnumerable<Indicator> Get()
         {
             var list = new List<Indicator>();
-            list.Add(new Indicator() { id = "0", mass = 400, minValue = 40, maxValue = 360, title="Бла бла 1", value = 380 });//красн
-            list.Add(new Indicator() { id = "1", mass = 800, minValue = 60, maxValue = 700, title = "Бла бла 2", value = 750 }); //красн
-            list.Add(new Indicator() { id = "2", mass = 300, minValue = 100, maxValue = 270, title = "Бла бла 3", value = 220 });
-            list.Add(new Indicator() { id = "3", mass = 100, minValue = 20, maxValue = 80, title = "Бла бла 4", value = 10 }); //красн
-            list.Add(new Indicator() { id = "4", mass = 1000, minValue = 150, maxValue = 900, title = "Бла бла 5", value = 700 }); 
-            list.Add(new Indicator() { id = "5", mass = 700, minValue = 40, maxValue = 650, title = "Бла бла 6", value = 380 });
-            list.Add(new Indicator() { id = "6", mass = 500, minValue = 40, maxValue = 400, title = "Бла бла 7", value = 20 }); // красн
-            list.Add(new Indicator() { id = "7", mass = 400, minValue = 40, maxValue = 400, title = "Бла бла 8", value = 380 });
+            //list.Add(new Indicator() { id = "0", mass = 400, minValue = 40, maxValue = 360, title = "Бла бла 1", value = 380 });//красн
+            //list.Add(new Indicator() { id = "1", mass = 800, minValue = 60, maxValue = 700, title = "Бла бла 2", value = 750 }); //красн
+            //list.Add(new Indicator() { id = "2", mass = 300, minValue = 100, maxValue = 270, title = "Бла бла 3", value = 220 });
+            //list.Add(new Indicator() { id = "3", mass = 100, minValue = 20, maxValue = 80, title = "Бла бла 4", value = 10 }); //красн
+            //list.Add(new Indicator() { id = "4", mass = 1000, minValue = 150, maxValue = 900, title = "Бла бла 5", value = 700 });
+            //list.Add(new Indicator() { id = "5", mass = 700, minValue = 40, maxValue = 650, title = "Бла бла 6", value = 380 });
+            //list.Add(new Indicator() { id = "6", mass = 500, minValue = 40, maxValue = 400, title = "Бла бла 7", value = 20 }); // красн
+            //list.Add(new Indicator() { id = "7", mass = 400, minValue = 40, maxValue = 400, title = "Бла бла 8", value = 380 });
+            for (int i = 2; i < 10; i++)
+            {
+                list.Add(GetRandom(i.ToString()));
+            }
             return list;
+        }
+
+        private Indicator GetRandom(string id)
+        {
+            var rand = new Random(int.Parse(id) * DateTime.Now.Second);
+            var temp = new Indicator() { id = id, mass = 1000, minValue = rand.Next(0, 200), maxValue = rand.Next(800, 1000), title = "someTitle", value = rand.Next(0, 1000) };
+            return temp;
         }
     }
 }
